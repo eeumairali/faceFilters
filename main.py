77a70374a45a917a207398f16ecb37930a56b3ba
umair <eeumairali@gmail.com>
2025-04-17 17:23:20 +0500

    updated main

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from tkinter import Tk
+import os
+import tkinter as tk
 from app_parts.video_processor import VideoProcessor
 from app_parts.app import App
 
 
 def main():
+    # Create root window
+    root = tk.Tk()
+    
+    # Set window icon if available
+    try:
+        root.iconbitmap("icons/app_icon.ico")
+    except:
+        pass  # Icon not found, use default
+        
+    # Create processor and app
     processor = VideoProcessor()
-    root = Tk()
     app = App(root, processor)
+    
+    # Start main loop
     root.mainloop()
 
 
